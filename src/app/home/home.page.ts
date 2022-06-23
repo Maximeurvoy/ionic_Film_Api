dcee6a3f61d29eb082e1bb03059e840593f5ed61
unknown <Administrateur@25SE23-3Q0TVD3.ad.campus-eni.fr>
2022-06-23 16:33:58 +0200

    ajoute une fausse pagination

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,13 +15,34 @@ export class HomePage {
   films: any[]=[];
   favorites: any[]=[];
   displayType ='favoris';
+  valPlus = '';
+  page = 0;
   constructor(private http: HttpClient) {}
 
   onRecherche() {
+    this.page ++;
     const val = this.film;
+    this.valPlus = this.film;
     const url = 'http://www.omdbapi.com/?apikey=efdc2275&s=' + val;
     this.film = '';
     console.log(url);
+    this.http.get<any>(url).subscribe((film) => {
+      // JSON
+      this.films = film.Search;
+      console.log(film);
+      console.log(this.films);
+      // this.nomFilm = film.Title;
+      // this.anneeFilm = film.Year;
+      // this.image = film.Poster;
+    });
+    this.displayType ='resultats';
+  }
+
+  onRecherchePlus() {
+    this.page ++;
+    const val = this.valPlus;
+    const url = 'http://www.omdbapi.com/?apikey=efdc2275&page='+ this.page +'&s=' + val;
+
     this.http.get<any>(url).subscribe((film) => {
       // JSON
       this.films = film.Search;

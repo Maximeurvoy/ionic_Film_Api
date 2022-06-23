651c086e0988cc990cc4bd2469898e0e51cd2d10
unknown <Administrateur@25SE23-3Q0TVD3.ad.campus-eni.fr>
2022-06-23 15:32:27 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // http
+  image = '';
+  film = '';
+  nomFilm = '';
+  anneeFilm = '';
+  films: any[]=[];
+  favorites: any[]=[];
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+  onRecherche() {
+    const val = this.film;
+    const url = 'http://www.omdbapi.com/?apikey=efdc2275&s=' + val;
+    this.film = '';
+    console.log(url);
+    this.http.get<any>(url).subscribe((film) => {
+      // JSON
+      this.films = film.Search;
+      console.log(film);
+      console.log(this.films);
+      // this.nomFilm = film.Title;
+      // this.anneeFilm = film.Year;
+      // this.image = film.Poster;
+    });
+  }
 
+  onFavorite(film: string){
+    this.favorites.push(film);
+    console.log( this.favorites);
+  }
 }
